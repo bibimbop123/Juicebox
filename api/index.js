5bcf6e95f67b07d67d0620b8d7dc4d698d050f02
briankim <brianhkim13@gmail.com>
2023-05-18 11:12:38 -0500

    dealing with Errors part 2 of part 2 webservers completed

--- api/index.js
+++ api/index.js
@@ -1,13 +1,62 @@
 // api/index.js
-const express = require('express');
+require("dotenv").config();
+
+// api/index.js
+// Before we start attaching our routers
+
+const jwt = require("jsonwebtoken");
+const { getUserById } = require("../db");
+const { JWT_SECRET } = process.env;
+const express = require("express");
 const apiRouter = express.Router();
 
-const usersRouter = require('./users');
-apiRouter.use('/users', usersRouter);
-const postsRouter = require('./posts');
-apiRouter.use('/posts', postsRouter);
-const tagsRouter = require('./tags');
-apiRouter.use('/tags', tagsRouter)
+apiRouter.use((req, res, next) => {
+  console.log("-----A REQUEST HAS BEEN MADE TO /API-----");
+  next();
+});
 
+// Attach routers below here
+apiRouter.use(async (req, res, next) => {
+  console.log("In the Middleware");
+  const prefix = "Bearer ";
+  const auth = req.header("Authorization");
 
-module.exports = apiRouter, postsRouter, tagsRouter;
+  if (!auth) {
+    // nothing to see here
+    next();
+  } else if (auth.startsWith(prefix)) {
+    const token = auth.slice(prefix.length);
+
+    try {
+      const { id } = jwt.verify(token, JWT_SECRET);
+
+      if (id) {
+        req.user = await getUserById(id);
+        next();
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  } else {
+    next({
+      name: "AuthorizationHeaderError",
+      message: `Authorization token must start with ${prefix}`,
+    });
+  }
+});
+
+const usersRouter = require("./users");
+apiRouter.use("/users", usersRouter);
+const postsRouter = require("./posts");
+apiRouter.use("/posts", postsRouter);
+const tagsRouter = require("./tags");
+apiRouter.use("/tags", tagsRouter);
+
+apiRouter.use((error, req, res, next) => {
+  res.send({
+    name: error.name,
+    message: error.message,
+  });
+});
+
+module.exports = apiRouter;
--- index.js
+++ index.js
@@ -1,39 +1,26 @@
 const PORT = 3000;
-const express = require('express');
+const express = require("express");
 const server = express();
-const apiRouter = require('./api');
-const morgan = require('morgan');
+const apiRouter = require("./api");
+const morgan = require("morgan");
 
-const { client } = require('./db');
+const { client } = require("./db");
 client.connect();
 
+server.use(morgan("dev"));
 
-server.listen(PORT, () => {
-  console.log('The server is up on port', PORT)
-});
-
-
-server.use(morgan('dev'));
-
-server.use(express.json())
+server.use(express.json());
 
 server.use((req, res, next) => {
-    console.log("<____Body Logger START____>");
-    console.log(req.body);
-    console.log("<_____Body Logger END_____>");
-  
-    next();
-  });
+  console.log("<____Body Logger START____>");
+  console.log(req.body);
+  console.log("<_____Body Logger END_____>");
 
-server.use('/api', (req, res, next) => {
-    console.log("A request was made to /api");
-    next();
-  });
-  
-server.get('/api', (req, res, next) => {
-    console.log("A get request was made to /api");
-    res.send({ message: "success" });
-  });
+  next();
+});
 
-server.use('/api', apiRouter);
+server.use("/api", apiRouter);
 
+server.listen(PORT, () => {
+  console.log("The server is up on port", PORT);
+});
